4898e6dc3d1ac5d0e94c9669af96e2b7fd17343f
loevray <rei980213@gmail.com>
2024-02-18 20:55:59 +0900

    Feat: add stop debounce

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,9 +1,13 @@
-const debounce = (callback, delay = 1000) => {
+export const debounce = (callback, delay = 1000) => {
   let timerId = null;
-  return (...args) => {
+  const _debounce = (...args) => {
     clearTimeout(timerId);
-    timerId = setTimeout(() => {
-      callback.apply(this, args);
-    }, delay);
+    timerId = setTimeout(() => callback.apply(this, args), delay);
   };
+
+  _debounce.stop = () => {
+    clearTimeout(timerId);
+  };
+
+  return _debounce;
 };
